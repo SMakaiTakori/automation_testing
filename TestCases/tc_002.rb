b81a95df5e75bfbdb9a2558fe51cf61d7aea1316
Makai Takori <makait87@icloud.com>
2018-07-18 20:41:30 -0400

    completed ruby testing tutorial

---
+++ TestCases/tc_002.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+require 'selenium-webdriver'
+require '../Base/base'
+require '../Pages/login'
+require '../Assertions/assert'
+
+class TC_002 < TestAutomationBase
+
+  def test_TestCase2
+
+  end
+
+  end
